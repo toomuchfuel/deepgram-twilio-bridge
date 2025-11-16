79f7c0376ebb7ca8511c77e8c1ed6e12fcdc3f1f
toomuchfuel <toomuchfuel@gmail.com>
2025-11-16 18:10:51 +1000

    Remove unsupported endpointing parameters from Voice Agent API

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import websockets
 import os
 import signal
 from aiohttp import web
-from config import VOICE_AGENT_PERSONALITY, VOICE_MODEL, LLM_MODEL, LLM_TEMPERATURE, ENDPOINTING_MS, UTTERANCE_END_MS, INTERIM_RESULTS
+from config import VOICE_AGENT_PERSONALITY, VOICE_MODEL, LLM_MODEL, LLM_TEMPERATURE
 
 # Global variables
 shutdown_event = asyncio.Event()
@@ -66,10 +66,7 @@ async def twilio_handler(twilio_ws):
                     "listen": {
                         "provider": {
                             "type": "deepgram",
-                            "model": "nova-3",
-                            "endpointing": ENDPOINTING_MS,
-                            "utterance_end_ms": UTTERANCE_END_MS,
-                            "interim_results": INTERIM_RESULTS
+                            "model": "nova-3"
                         }
                     },
                     "think": {
